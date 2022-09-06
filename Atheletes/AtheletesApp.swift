ace50758fd4c9567bcb1d470d6c96c2baac1badf
Norman <normanndot@gmail.com>
2022-09-06 14:50:58 +0530

    Initial Commit

---
+++ Atheletes/AtheletesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtheletesApp.swift
+//  Atheletes
+//
+//  Created by Norman, ThankaVijay on 06/09/22.
+//
+
+import SwiftUI
+
+@main
+struct AtheletesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
